171b077508186128a8deec8b2256bf0004013915
walkman1001 <75269908@qq.com>
2022-09-03 19:22:32 +0800

    第一次引入gin框架

--- study_gin/main.go
+++ study_gin/main.go
@@ -1,9 +1,22 @@
 package main
 
 import (
-	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
-func main(){
-	fmt.Println("hello_word")
+func HelloWord() {
+	//1.创建路由
+	r := gin.Default()
+	//2.绑定路由规则，执行的函数
+	r.GET("/", func(context *gin.Context) {
+		context.String(http.StatusOK, "Hello World!")
+	})
+	//3.监听端口，默认8080
+	r.Run(":8080")
+}
+
+func main() {
+	HelloWord()
 }
